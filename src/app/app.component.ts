2dfb3091b3520757744736810c98480df5b8c41a
Lucas Ramos <lucas.ramos@gft.com>
2023-04-09 09:55:33 -0300

    feat: add service with mock delay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 import { App } from './models/app.model';
+import { AppService } from './services/app-service.service';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +9,9 @@ import { App } from './models/app.model';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  app: App = {
-    appName: 'This is my first app',
-    appVersion: '7.0',
-    appContact: 'This is the app contact information',
-  };
+  appList$: Observable<Array<App>>;
+
+  constructor(private appService: AppService) {
+    this.appList$ = this.appService.apps;
+  }
 }
